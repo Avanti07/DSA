7966ec377d1421a855e03ce7b46617ad90e056c5
Avanti Gharote <58411856+Avanti07@users.noreply.github.com>
2022-01-25 01:54:30 +0530

    Create LinkedList_DeleteAtPosition.java

---
+++ LinkedList_DeleteAtPosition.java
@@ -0,0 +1,94 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+import java.util.*;
+
+public class Main
+{
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        LinkedList l = new LinkedList();
+        //Node head = new Node(0);
+        l.add(1);
+        l.add(2);
+        l.add(5);
+        l.add(6);
+        l.add(7);
+        l.printList(); //prints 7->6->5->2->1
+        
+        l.addNodeAtPos(l.getHead(), 10, 2); 
+        l.printList(); //prints 7->6->10->5->2->1
+        
+        l.deleteAtPos(l.getHead(), 2);
+        l.printList(); //prints 7->6->5->2->1
+        
+    }
+}
+
+class LinkedList{
+  
+  Node head;  
+
+    public class Node
+    {
+        int data;
+        Node next;
+    
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    //adds new node at the start
+    public void add(int val){
+        Node newNode = new Node(val);
+        newNode.next = head;
+        this.head = newNode;
+    }
+    
+    //returns the head node
+    public Node getHead(){
+        return this.head;
+    }
+    
+    //delete a node from given position
+    public void deleteAtPos(Node head, int position){
+        Node temp = head;
+        for(int i =0; i< position-1; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+    
+    //Add new element at the given position
+    public void addNodeAtPos(Node head, int val,int pos){
+        Node toAdd = new Node(val);
+        Node cur = head;
+        if(pos == 0){
+            toAdd.next = head;
+            head = toAdd;
+            return;
+        }
+        else{
+            for(int i=0; i<pos-1; i++){
+                cur = cur.next;
+            }
+            toAdd.next = cur.next;
+            cur.next = toAdd;
+        }
+        return;
+    }
+    
+    public void printList(){
+        Node temp =head;
+        while(temp.next != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+
+}
